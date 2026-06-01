d69b5db378c6dec053de4c8411fe0297479205b1
bhimlex13 <oroalexander004@gmail.com>
2026-06-02 02:12:58 +0800

    Hotfix: Fix API auth token interception and CSS light mode overrides

--- client/src/api.js
+++ client/src/api.js
@@ -7,6 +7,17 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' }
 });
 
+// Add a request interceptor to attach the JWT token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 // Products
 export const fetchProducts = (params) => api.get('/products', { params });
 export const fetchProduct = (id) => api.get(`/products/${id}`);
